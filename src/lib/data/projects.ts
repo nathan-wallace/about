918d0f82f33ea1af77bdd718270fdb4d7c402016
Nathan Wallace <nathan.s.wallace@gmail.com>
2025-09-29 10:11:55 -0400

    updated project data

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -7,6 +7,13 @@ export const projects = [
     skills: ['D3.js']
   },
   {
+    title: 'Social Design Kit',
+    description: 'Kickstart social media campaigns with this GitHub template built on Storybook, letting teams create, preview, and export on-brand assets, drag-and-drop helpers, and ready-to-customize platform canvases.',
+    link: 'https://github.com/nathan-wallace/social-toolkit-starter',
+    image: '/social-design-kit.png',
+    skills: ['Storybook', 'Design Systems', 'JavaScript']
+  },
+  {
     title: 'SpeakEasy for Drupal',
     description: 'Using the built-in browser features for TTS, this module provides a seamless way to convert written content into speech without the need for external services.',
     link: 'https://www.drupal.org/project/speakeasy',
